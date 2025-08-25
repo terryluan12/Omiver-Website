3aa616e69f491022d560a192171cfffc07c5404f
Terry <terryluan12@gmail.com>
2025-08-25 00:26:13 -0400

    First draft for profile_detail page

--- demo/urls.py
+++ demo/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("select", views.select_page, name="select_page"),
+    path("profile_detail/<int:id>", views.profile_detail, name="profile_detail")
 ]
--- demo/views.py
+++ demo/views.py
@@ -7,3 +7,11 @@ def select_page(request):
     with open(DATA_FILE) as f:
         profile_data = json.load(f)
     return render(request, "demo/select.html", {"profiles": profile_data})
+
+def profile_detail(request, id):
+    DATA_FILE = os.path.join(settings.BASE_DIR, "demo", "data", "profiles.json")
+    with open(DATA_FILE) as f:
+        all_profiles = json.load(f)
+    profile_data = all_profiles[id]
+    
+    return render(request, "demo/profile_detail.html", profile_data)
