c3ec349fcd9ca77e2376c76727ad4dd75458f87a
Terry <terryluan12@gmail.com>
2025-09-02 13:15:10 -0400

    Moved welcome page to extend a two_column_layout.html template

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("support", views.support, name="select_page"),
+    path("support", views.support, name="support_page"),
     path("ticket_detail_partial/<int:pk>", views.ticket_detail_partial, name="ticket_detail_partial"),
     path("settings", views.settings, name="settings_page"),
     path("", views.dashboard, name="dashboard_base")
