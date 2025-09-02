d38811fb9419147d1a599793a174762ad929192a
Terry <terryluan12@gmail.com>
2025-09-02 02:45:39 -0400

    Moved welcome page to demo app

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("welcome", views.welcome_page, name="welcome_page"),
     path("login", views.login_page, name="login_page"),
 ]
